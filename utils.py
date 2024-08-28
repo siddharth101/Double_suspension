f4ef4c3ac3d1e2dc3e1b5d7ce1c379c4ea19c3a0
Siddharth Soni <siddharth.soni@ldas-pcdev5.ligo.caltech.edu>
2024-08-28 10:18:06 -0700

    Minor changes in utils and wire

--- utils.py
+++ utils.py
@@ -1,35 +1,150 @@
-from sympy.physics.mechanics import dynamicsymbols, ReferenceFrame
-from sympy.physics.mechanics import ReferenceFrame, Point, RigidBody
-from sympy import zeros, symbols
-
-import numpy as np
-import sympy as sp
-import sympy.physics.mechanics as me
-from IPython.display import display
-from suspycious import Model
+from sympy.physics.mechanics import Point, RigidBody
+from sympy import zeros, symbols, Matrix
 import suspycious.components as scmp
-
-k1, k2, k3, k4,  g, c1, c2, t = symbols('k1 k2 k3 k4 g c1 c2 t')
-
-from sympy.physics.mechanics import dynamicsymbols, ReferenceFrame
-from sympy import zeros, symbols
-
-import numpy as np
+from pathway import LinearPathway
 import sympy as sp
-import sympy.physics.mechanics as me
-from IPython.display import display
-from suspycious import Model
-
-d1, d2, n1, n2, w1, w2 = symbols('d1 d2 n1 n2 w1 w2', real=True, positive=True)
-d3, n3, w3 = symbols('d3 n3 w3', real=True, positive=True)
-d4, n4, w4 = symbols('d4 n4 w4', real=True, positive=True)
-d5, n5, w5 = symbols('d5 n5 w5', real=True, positive=True)
-d6, n6, w6 = symbols('d6 n6 w6', real=True, positive=True)
-d7, n7, w7 = symbols('d7 n7 w7', real=True, positive=True)
-d8, d9 = symbols('d8 d9', real=True, positive=True)
+import numpy as np
 
 
-def add_points(body, point='P', attachment_points=[w1, n1, d1]):
+def get_tension_dirs(body1, body2, points_1='top',
+                     points_2='top', suspension_body=None):
+
+    body1_points = list(body1.points.values())
+    body2_points = list(body2.points.values())
+
+    if points_1 == 'top':
+        body1_points = body1_points[:4]
+    else:
+        body1_points = body1_points[4:]
+    if points_2 == 'top':
+        body2_points = body2_points[:4]
+    else:
+        body2_points = body2_points[4:]
+
+    print(body1_points)
+    print(body2_points)
+    S = suspension_body
+    ten_dirs = [i.pos_from(j).express(S.global_frame).normalize() for i, j in list(zip(body1_points, body2_points))]
+
+    return ten_dirs
+
+
+def get_wire_dir(body1, body2, points_1='top',
+                     points_2='top', suspension_body=None):
+
+    paths = get_linpaths(body1, body2, points_1=points_1,
+                     points_2=points_2, suspension_body=suspension_body)['paths']
+
+    S = suspension_body
+
+    dirs = [i.attachments[0].pos_from(i.attachments[1]).express(S.global_frame).normalize() for i in paths]
+
+    return dirs
+
+def get_linpaths(body1, body2, points_1='top',
+                     points_2='top', suspension_body=None):
+
+    body1_points = list(body1.points.values())
+    body2_points = list(body2.points.values())
+
+    if points_1 == 'top':
+        body1_points = body1_points[:4]
+    else:
+        body1_points = body1_points[4:]
+    if points_2 == 'top':
+        body2_points = body2_points[:4]
+    else:
+        body2_points = body2_points[4:]
+
+    print(body1_points)
+    print(body2_points)
+    S = suspension_body
+    linpaths = {'paths':[LinearPathway(i,j) for i, j in list(zip(body1_points, body2_points))]} | {'bodies':[body1.body, body2.body]}
+    
+    
+    #linpaths.bodies = [body1, body2]
+    
+    return linpaths
+
+
+
+def give_deltas(body1, body2, suspension_body,
+                model, points_1='top', points_2='top'):
+
+    body1_points = list(body1.points.values())
+    body2_points = list(body2.points.values())
+
+    if points_1 == 'top':
+        body1_points = body1_points[:4]
+    else:
+        body1_points = body1_points[4:]
+    if points_2 == 'top':
+        body2_points = body2_points[:4]
+    else:
+        body2_points = body2_points[4:]
+
+    points_ = list(zip(body1_points, body2_points))
+
+    op_point = model.op_point
+    sb = suspension_body
+    print(points_)
+    deltas_ = [i.pos_from(j).express(sb.global_frame).magnitude() - i.pos_from(j).express(sb.global_frame).subs(op_point).magnitude() for i, j in points_]
+    return deltas_
+
+
+def add_attachment_points(name, body, position='top', *args, **kwargs):
+    points = [Point('{}{}'.format(name, j)) for j in range(1, 9)]
+    points_ = [str(i) for i in points]  # these are the points
+    matrix_attachment = Matrix([(1, -1, 1), (1, 1, 1), (-1, -1, 1), (-1, 1, 1),
+                                (1, -1, -1), (1, 1, -1), (-1, -1, -1), (-1, 1, -1)])
+
+    if position == 'top':
+        for i in range(4):
+            body._unprotect()
+            pt = points_[i]
+            dx_pt = body._add_symbol(f"{pt}_x", f"{pt}_x", level=0, real=True)
+            dy_pt = body._add_symbol(f"{pt}_y", f"{pt}_y", level=0, real=True)
+            dz_pt = body._add_symbol(f"{pt}_z", f"{pt}_z", level=0, real=True)
+            attachment_points = [dx_pt, dy_pt, dz_pt]
+            matrix_col_1 = matrix_attachment[i:i+1, 0]*attachment_points[0]
+            matrix_col_2 = matrix_attachment[i:i+1, 1]*attachment_points[1]
+            matrix_col_3 = matrix_attachment[i:i+1, 2]*attachment_points[2]
+            a = [matrix_col_1[0, 0], matrix_col_2[0, 0], matrix_col_3[0, 0]]
+            print(a[0], a[1], a[2])
+            body.add_fixed_point(points_[i], a[0], a[1], a[2])
+
+    else:
+        for i in range(4, 8):
+            body._unprotect()
+            pt = points_[i]
+            dx_pt = body._add_symbol(f"{pt}_x", f"{pt}_x", level=0, real=True)
+            dy_pt = body._add_symbol(f"{pt}_y", f"{pt}_y", level=0, real=True)
+            dz_pt = body._add_symbol(f"{pt}_z", f"{pt}_z", level=0, real=True)
+            attachment_points = [dx_pt, dy_pt, dz_pt]
+
+            matrix_col_1 = matrix_attachment[i:i+1, 0]*attachment_points[0]
+            matrix_col_2 = matrix_attachment[i:i+1, 1]*attachment_points[1]
+            matrix_col_3 = matrix_attachment[i:i+1, 2]*attachment_points[2]
+            a = [matrix_col_1[0, 0], matrix_col_2[0, 0], matrix_col_3[0, 0]]
+            print(a[0], a[1], a[2])
+            body.add_fixed_point(points_[i], a[0], a[1], a[2])
+
+    body._protect()
+
+    
+    
+def give_tensions(n_body, k, masses, delta_values=None):
+    
+    n = n_body -1
+    g = symbols('g')
+    #masses = [B.M, C.M, D.M, F.M]
+    masses_ = masses[n:]
+    
+    tensions = [sp.Rational(1,4)*(np.sum(masses_))*g + k*i for i in delta_values]
+        
+    return tensions 
+
+def add_points(body, point, attachment_points):
     points = [Point('{}{}'.format(point,j)) for j in range(1,5)]
     
     points_ = [str(i) for i in points]
@@ -46,60 +161,25 @@ def add_points(body, point='P', attachment_points=[w1, n1, d1]):
 
     return
 
-def get_tension_dirs(body1, body2, suspension_body, points_1='top', points_2='top'):
+def apply_force(body, forcedict, global_frame, bottom=True):
+   
+    top_path = forcedict[body]['paths'][0]['paths']
+    top_points = list(forcedict[body]['points'].values())[:4]
+    top_tensions = forcedict[body]['tensions'][:4]
     
-    body1_points = list(body1.points.values())
-    body2_points = list(body2.points.values())
+    for i in range(4):
+        body.body.apply_force(top_path[i].to_loads(-top_tensions[i], frame=global_frame)[1][1], point=top_points[i])
     
-    if points_1 == 'top':
-        body1_points = body1_points[:4]
-    else:
-        body1_points = body1_points[4:]
-    if points_2 == 'top':
-        body2_points = body2_points[:4]
-    else:
-        body2_points = body2_points[4:]
+    if bottom:
+        bottom_path = forcedict[body]['paths'][1]['paths']
+        bottom_points = list(forcedict[body]['points'].values())[4:]
+        bottom_tension = forcedict[body]['tensions'][4:]
         
-    print(body1_points)
-    print(body2_points)
-    
-    ten_dirs = [i.pos_from(j).express(suspension_body.global_frame).normalize() for i,j in list(zip(body1_points, body2_points))]
-        
-    return ten_dirs
-    
-    
-def give_deltas(body1, body2, suspension_body,model,points_1='top', points_2='top'):
-    
-    body1_points = list(body1.points.values())
-    body2_points = list(body2.points.values())
-    
-    if points_1 == 'top':
-        body1_points = body1_points[:4]
+        for i in range(4):
+            body.body.apply_force(bottom_path[i].to_loads(-bottom_tension[i], frame=global_frame)[0][1], point=bottom_points[i])
     else:
-        body1_points = body1_points[4:]
-    if points_2 == 'top':
-        body2_points = body2_points[:4]
-    else:
-        body2_points = body2_points[4:]
-        
-    points_ = list(zip(body1_points, body2_points))
-    
-    op_point = model.op_point
-    sb = suspension_body
-    print(points_)
-    
-    deltas_ = [i.pos_from(j).express(sb.global_frame).magnitude() - i.pos_from(j).express(sb.global_frame).subs(op_point).magnitude() 
-              for i,j in points_]
-    return deltas_
+        pass
     
     
-def give_tensions(masses, n_body=1, k=k1, delta_values=None ):
     
-    n = n_body -1
-    
-    #masses = [B.M, C.M, D.M, F.M]
-    masses_ = masses[n:]
-    
-    tensions = [sp.Rational(1,4)*(np.sum(masses_))*g + k*i for i in delta_values]
-        
-    return tensions 
+    return
--- wire.py
+++ wire.py
@@ -8,6 +8,7 @@ F, M, g, t = symbols('F, M, g, t')
 def make_wire(n):
     '''n defines the number of small masses we want to add in the wire''' 
     
+    L = symbols('L', positive=True)
     # Creating Reference frames, angles, coordinates and speeds
     N = ReferenceFrame('N')
     frames = symbols('N1:{}'.format(n+2), cls=ReferenceFrame)
@@ -94,4 +95,112 @@ def make_wire(n):
     
     A, B = kanel.linearize(A_and_B=True, op_point=op_vals)
     
+    A = A.subs({l1:L/(n+1)})
+    
     return A
+
+
+def make_wire_3d(n):
+    '''n defines the number of small masses we want to add in the wire''' 
+    
+    
+    L = symbols('L', positive=True)
+    # Creating Reference frames, angles, coordinates and speeds
+    N = ReferenceFrame('N')
+    frames = symbols('N1:{}'.format(n+2), cls=ReferenceFrame)
+    angles = dynamicsymbols('alpha1:{}'.format(n+2))
+    betas = symbols('beta1:{}'.format(n+1))
+    qs = dynamicsymbols('q1:{}'.format(3*(n+1)+1))
+    us = dynamicsymbols('u1:{}'.format(3*(n+1)+1))
+    
+    masses = symbols('m1:{}'.format(n+1))
+    M = symbols('M')
+    Mf = symbols('Mf')
+   
+    #Creating the Points
+    O = Point('O')
+    ps = symbols('P1:{}'.format(n+2), cls=Point)
+    print(ps)
+
+    # Setting these points in space
+    j=0
+    for i in range(n+1):
+        #print(i)
+        ps[i].set_pos(O, qs[j]*N.x + qs[j+1]*N.y + qs[j+2]*N.z)
+        j+=3
+        
+    
+    print(qs) 
+    
+    
+    # Creating Wall and adding it to the system 
+    wall = RigidBody('W', masscenter=O, frame=N)
+    system = System.from_newtonian(wall)
+   
+    # Aligning N with system frame
+    N.orient_axis(system.frame, 0, system.frame.z)
+    
+    # Rotating the frames wrt system frame  
+    [i.orient_axis(system.frame, j, system.frame.z) for i,j in list(zip(frames, angles))]
+
+    # Creating the bodies
+    bodies = [RigidBody('B{}'.format(h), mass=i*j, masscenter=k, frame=l) for h,i,j,k,l in list(zip(range(1,n+1), betas,masses, ps[:-1], frames[:-1]))]
+
+    last_body = RigidBody('B{}'.format(n+1), mass=M, masscenter=ps[-1], frame=frames[-1])
+    
+    bodies.append(last_body)
+    print(bodies)
+    # Adding bodies to the system
+    for i in bodies:
+        system.add_bodies(i)
+       
+    #system.add_bodies(last_body)
+    
+    # Assigning velocities to the bodies
+    j = 0
+    for i in range(n+1):
+        #print(frames[i], bodies[i])
+        bodies[i].masscenter.set_vel(system.frame, us[j]*N.x + us[j+1]*N.y + us[j+2]*N.z)
+        bodies[i].frame.set_ang_vel(frames[i], 0)
+        j+=3
+        
+    # Adding co-ordinates, speeds and kdes to the system 
+    for k in list(zip(qs, us)):
+        system.add_coordinates(k[0])
+        system.add_speeds(k[1])
+        system.add_kdes(k[1]-k[0].diff(t))
+       
+    # Applying gravity
+    system.apply_uniform_gravity(-g * wall.z)
+   
+    # Now we will create linearpathways between the points and apply force along them
+    linear_points = [O] + [i for i in ps]
+    print(linear_points)
+    
+    linear_paths = []
+    for i in range(1, len(linear_points)-1):
+        lin_paths_a = LinearPathway(linear_points[i-1], linear_points[i])
+        lin_paths_b = LinearPathway(linear_points[i+1], linear_points[i])
+        linear_paths.append(lin_paths_a)
+        linear_paths.append(lin_paths_b)
+    linear_paths.append(LinearPathway(linear_points[-2], linear_points[-1]))
+    print(linear_paths)
+    
+    [system.add_loads(i.to_loads(-F)[1]) for i in linear_paths[:-1]]
+   
+
+    # System is prepared, we can validate it
+    system.validate_system()
+    
+    # Getting the kane's equations
+    system.form_eoms()
+    kanel = system.eom_method.to_linearizer()
+    #l1 = L/(n+1)
+    
+    op_vals = {qs[i]:0 for i in range(0,3*(n+1),3)} | {qs[i]:0 for i in range(1,3*(n+1),3)} |  {qs[i]:-(i-i//3)*l1/2 for i in range(2,3*(n+1),3)} | {F:M*g}
+    A, B = kanel.linearize(A_and_B=True, op_point=op_vals)
+    
+    A = A.subs({l1:L/(n+1)})
+    
+    return A, system
+
