d212652a4405ac49e5c913c4871b485b7d8e7cfc
Siddharth Soni <siddharth.soni@ldas-pcdev5.ligo.caltech.edu>
2024-09-02 07:44:06 -0700

    pep8 compatibility

--- wire.py
+++ wire.py
@@ -1,206 +1,215 @@
-from sympy import zeros, symbols
-from sympy.physics.mechanics import dynamicsymbols, ReferenceFrame
-from sympy.physics.mechanics import ReferenceFrame, Point, RigidBody, Particle, System,  Force
+from sympy import symbols
+from sympy.physics.mechanics import dynamicsymbols, ReferenceFrame, Point, RigidBody, System
 from pathway import LinearPathway
 
 l1 = symbols('l1', real=True, positive=True)
 F, M, g, t = symbols('F, M, g, t')
+
+
 def make_wire(n):
-    '''n defines the number of small masses we want to add in the wire''' 
-    
+    """n defines the number of small masses we want to add in the wire"""
+
     L = symbols('L', positive=True)
-    # Creating Reference frames, angles, coordinates and speeds
+    # Creating Reference frames, angles, coordinates, and speeds
     N = ReferenceFrame('N')
-    frames = symbols('N1:{}'.format(n+2), cls=ReferenceFrame)
-    angles = dynamicsymbols('alpha1:{}'.format(n+2))
-    betas = symbols('beta1:{}'.format(n+2))
-    qs = dynamicsymbols('q1:{}'.format(2*(n+1)+1))
-    us = dynamicsymbols('u1:{}'.format(2*(n+1)+1))
-    
-    masses = symbols('m1:{}'.format(n+2))
+    frames = symbols(f'N1:{n + 2}', cls=ReferenceFrame)
+    angles = dynamicsymbols(f'alpha1:{n + 2}')
+    betas = symbols(f'beta1:{n + 2}')
+    qs = dynamicsymbols(f'q1:{2 * (n + 1) + 1}')
+    us = dynamicsymbols(f'u1:{2 * (n + 1) + 1}')
+    masses = symbols(f'm1:{n + 2}')
     M = symbols('M')
-   
-    #Creating the Points
+
+    # Creating the Points
     O = Point('O')
-    ps = symbols('P1:{}'.format(n+2), cls=Point)
+    ps = symbols(f'P1:{n + 2}', cls=Point)
     print(ps)
 
     # Setting these points in space
-    j=0
-    for i in range(n+1):
-        #print(i)
-        ps[i].set_pos(O, qs[j]*N.x + qs[j+1]*N.y)
-        j+=2
-        
-    
-    # Creating Wall and adding it to the system 
+    j = 0
+    for i in range(n + 1):
+        ps[i].set_pos(O, qs[j] * N.x + qs[j + 1] * N.y)
+        j += 2
+
+    # Creating Wall and adding it to the system
     wall = RigidBody('W', masscenter=O, frame=N)
     system = System.from_newtonian(wall)
-   
+
     # Aligning N with system frame
     N.orient_axis(system.frame, 0, system.frame.z)
-    
-    # Rotating the frames wrt system frame  
-    [i.orient_axis(system.frame, j, system.frame.z) for i,j in list(zip(frames, angles))]
+
+    # Rotating the frames wrt system frame
+    for i, j in zip(frames, angles):
+        i.orient_axis(system.frame, j, system.frame.z)
 
     # Creating the bodies
-    bodies = [RigidBody('B{}'.format(h), mass=i*j, masscenter=k, frame=l) for h,i,j,k,l in list(zip(range(1,n+2), betas,masses, ps, frames))]
-    
+    bodies = [
+        RigidBody(
+            f'B{h}', mass=i * j, masscenter=k, frame=l
+        ) for h, i, j, k, l in zip(range(1, n + 2), betas, masses, ps, frames)
+    ]
+
     # Adding bodies to the system
-    for i in bodies:
-        system.add_bodies(i)
-       
+    for body in bodies:
+        system.add_bodies(body)
 
     # Assigning velocities to the bodies
     j = 0
-    for i in range(n+1):
-        #print(frames[i], bodies[i])
-        bodies[i].masscenter.set_vel(system.frame, us[j]*N.x + us[j+1]*N.y)
+    for i in range(n + 1):
+        bodies[i].masscenter.set_vel(system.frame, us[j] * N.x + us[j + 1] * N.y)
         bodies[i].frame.set_ang_vel(frames[i], 0)
-        j+=2
-        
-    # Adding co-ordinates, speeds and kdes to the system 
-    for k in list(zip(qs, us)):
+        j += 2
+
+    # Adding coordinates, speeds, and kdes to the system
+    for k in zip(qs, us):
         system.add_coordinates(k[0])
         system.add_speeds(k[1])
-        system.add_kdes(k[1]-k[0].diff(t))
-       
+        system.add_kdes(k[1] - k[0].diff(t))
+
     # Applying gravity
     system.apply_uniform_gravity(-g * wall.y)
-   
-    # Now we will create linearpathways between the points and apply force along them
-    linear_points = [O] + [i for i in ps]
+
+    # Creating linear pathways between the points and applying force along them
+    linear_points = [O] + list(ps)
     print(linear_points)
-    
+
     linear_paths = []
-    for i in range(1, len(linear_points)-1):
-        lin_paths_a = LinearPathway(linear_points[i-1], linear_points[i])
-        lin_paths_b = LinearPathway(linear_points[i+1], linear_points[i])
+    for i in range(1, len(linear_points) - 1):
+        lin_paths_a = LinearPathway(linear_points[i - 1], linear_points[i])
+        lin_paths_b = LinearPathway(linear_points[i + 1], linear_points[i])
         linear_paths.append(lin_paths_a)
         linear_paths.append(lin_paths_b)
     linear_paths.append(LinearPathway(linear_points[-2], linear_points[-1]))
     print(linear_paths)
-    
-    [system.add_loads(i.to_loads(-F)[1]) for i in linear_paths]
-   
 
-    # System is prepared, we can validate it
+    for path in linear_paths:
+        system.add_loads(path.to_loads(-F)[1])
+
+    # Validating the system
     system.validate_system()
-    
-    # Getting the kane's equations
+
+    # Getting the Kane's equations
     system.form_eoms()
     kanel = system.eom_method.to_linearizer()
-    
-    op_vals = {qs[i]:0 for i in range(0,2*(n+1),2)} |  {qs[i]:-(i-i//2)*l1 for i in range(1,2*(n+1),2)} | {F:M*g}
-    
+
+    op_vals = {
+        qs[i]: 0 for i in range(0, 2 * (n + 1), 2)
+    } | {
+        qs[i]: -(i - i // 2) * l1 for i in range(1, 2 * (n + 1), 2)
+    } | {
+        F: M * g
+    }
+
     A, B = kanel.linearize(A_and_B=True, op_point=op_vals)
-    
-    A = A.subs({l1:L/(n+1)})
-    
+    A = A.subs({l1: L / (n + 1)})
+
     return A
 
 
 def make_wire_3d(n, force):
-    '''n defines the number of small masses we want to add in the wire''' 
-    
-    
+    """n defines the number of small masses we want to add in the wire"""
+
     L = symbols('L', positive=True)
-    # Creating Reference frames, angles, coordinates and speeds
+    # Creating Reference frames, angles, coordinates, and speeds
     N = ReferenceFrame('N')
-    frames = symbols('N1:{}'.format(n+2), cls=ReferenceFrame)
-    angles = dynamicsymbols('alpha1:{}'.format(n+2))
-    betas = symbols('beta1:{}'.format(n+1))
-    qs = dynamicsymbols('q1:{}'.format(3*(n+1)+1))
-    us = dynamicsymbols('u1:{}'.format(3*(n+1)+1))
-    
-    masses = symbols('m1:{}'.format(n+1))
+    frames = symbols(f'N1:{n + 2}', cls=ReferenceFrame)
+    angles = dynamicsymbols(f'alpha1:{n + 2}')
+    betas = symbols(f'beta1:{n + 1}')
+    qs = dynamicsymbols(f'q1:{3 * (n + 1) + 1}')
+    us = dynamicsymbols(f'u1:{3 * (n + 1) + 1}')
+    masses = symbols(f'm1:{n + 1}')
     M = symbols('M')
     Mf = symbols('Mf')
-   
-    #Creating the Points
+
+    # Creating the Points
     O = Point('O')
-    ps = symbols('P1:{}'.format(n+2), cls=Point)
+    ps = symbols(f'P1:{n + 2}', cls=Point)
     print(ps)
 
     # Setting these points in space
-    j=0
-    for i in range(n+1):
-        #print(i)
-        ps[i].set_pos(O, qs[j]*N.x + qs[j+1]*N.y + qs[j+2]*N.z)
-        j+=3
-        
-    
-    print(qs) 
-    
-    
-    # Creating Wall and adding it to the system 
+    j = 0
+    for i in range(n + 1):
+        ps[i].set_pos(O, qs[j] * N.x + qs[j + 1] * N.y + qs[j + 2] * N.z)
+        j += 3
+
+    print(qs)
+
+    # Creating Wall and adding it to the system
     wall = RigidBody('W', masscenter=O, frame=N)
     system = System.from_newtonian(wall)
-   
+
     # Aligning N with system frame
     N.orient_axis(system.frame, 0, system.frame.z)
-    
-    # Rotating the frames wrt system frame  
-    [i.orient_axis(system.frame, j, system.frame.z) for i,j in list(zip(frames, angles))]
+
+    # Rotating the frames wrt system frame
+    for i, j in zip(frames, angles):
+        i.orient_axis(system.frame, j, system.frame.z)
 
     # Creating the bodies
-    bodies = [RigidBody('B{}'.format(h), mass=i*j, masscenter=k, frame=l) for h,i,j,k,l in list(zip(range(1,n+1), betas,masses, ps[:-1], frames[:-1]))]
+    bodies = [
+        RigidBody(
+            f'B{h}', mass=i * j, masscenter=k, frame=l
+        ) for h, i, j, k, l in zip(range(1, n + 1), betas, masses, ps[:-1], frames[:-1])
+    ]
 
-    last_body = RigidBody('B{}'.format(n+1), mass=M, masscenter=ps[-1], frame=frames[-1])
-    
+    last_body = RigidBody(f'B{n + 1}', mass=M, masscenter=ps[-1], frame=frames[-1])
     bodies.append(last_body)
     print(bodies)
+
     # Adding bodies to the system
-    for i in bodies:
-        system.add_bodies(i)
-       
-    #system.add_bodies(last_body)
-    
+    for body in bodies:
+        system.add_bodies(body)
+
     # Assigning velocities to the bodies
     j = 0
-    for i in range(n+1):
-        #print(frames[i], bodies[i])
-        bodies[i].masscenter.set_vel(system.frame, us[j]*N.x + us[j+1]*N.y + us[j+2]*N.z)
+    for i in range(n + 1):
+        bodies[i].masscenter.set_vel(system.frame, us[j] * N.x + us[j + 1] * N.y + us[j + 2] * N.z)
         bodies[i].frame.set_ang_vel(frames[i], 0)
-        j+=3
-        
-    # Adding co-ordinates, speeds and kdes to the system 
-    for k in list(zip(qs, us)):
+        j += 3
+
+    # Adding coordinates, speeds, and kdes to the system
+    for k in zip(qs, us):
         system.add_coordinates(k[0])
         system.add_speeds(k[1])
-        system.add_kdes(k[1]-k[0].diff(t))
-       
+        system.add_kdes(k[1] - k[0].diff(t))
+
     # Applying gravity
     system.apply_uniform_gravity(-g * wall.z)
-   
-    # Now we will create linearpathways between the points and apply force along them
-    linear_points = [O] + [i for i in ps]
+
+    # Creating linear pathways between the points and applying force along them
+    linear_points = [O] + list(ps)
     print(linear_points)
-    
+
     linear_paths = []
-    for i in range(1, len(linear_points)-1):
-        lin_paths_a = LinearPathway(linear_points[i-1], linear_points[i])
-        lin_paths_b = LinearPathway(linear_points[i+1], linear_points[i])
+    for i in range(1, len(linear_points) - 1):
+        lin_paths_a = LinearPathway(linear_points[i - 1], linear_points[i])
+        lin_paths_b = LinearPathway(linear_points[i + 1], linear_points[i])
         linear_paths.append(lin_paths_a)
         linear_paths.append(lin_paths_b)
     linear_paths.append(LinearPathway(linear_points[-2], linear_points[-1]))
     print(linear_paths)
-    
-    [system.add_loads(i.to_loads(-F)[1]) for i in linear_paths[:-1]]
-   
 
-    # System is prepared, we can validate it
+    for path in linear_paths[:-1]:
+        system.add_loads(path.to_loads(-F)[1])
+
+    # Validating the system
     system.validate_system()
-    
-    # Getting the kane's equations
+
+    # Getting the Kane's equations
     system.form_eoms()
     kanel = system.eom_method.to_linearizer()
-    #l1 = L/(n+1)
-    
-    op_vals = {qs[i]:0 for i in range(0,3*(n+1),3)} | {qs[i]:0 for i in range(1,3*(n+1),3)} |  {qs[i]:-(i-i//3)*l1/2 for i in range(2,3*(n+1),3)} | {F:force}
-    A, B = kanel.linearize(A_and_B=True, op_point=op_vals)
-    
-    A = A.subs({l1:L/(n+1)})
-    
-    return A, system
 
+    op_vals = {
+        qs[i]: 0 for i in range(0, 3 * (n + 1), 3)
+    } | {
+        qs[i]: 0 for i in range(1, 3 * (n + 1), 3)
+    } | {
+        qs[i]: -(i - i // 3) * l1 / 2 for i in range(2, 3 * (n + 1), 3)
+    } | {
+        F: force
+    }
+
+    A, B = kanel.linearize(A_and_B=True, op_point=op_vals)
+    A = A.subs({l1: L / (n + 1)})
+
+    return A, system
--- wire_suspy.py
+++ wire_suspy.py
@@ -1,143 +1,136 @@
 from sympy import zeros, symbols
 from sympy.physics.mechanics import dynamicsymbols, ReferenceFrame
-from sympy.physics.mechanics import ReferenceFrame, Point, RigidBody, Particle, System, Force
+from sympy.physics.mechanics import Point, RigidBody, System, Force
 from pathway import LinearPathway
 import numpy as np
 
 
 def make_wire_suspy(n, linpath, index, global_frame, suspension_body):
-    
+    """
+    Function to create a wire system with a specified number of small masses
+    suspended between two points.
+
+    Parameters:
+    n (int): Number of small masses to add to the wire.
+    linpath (dict): Dictionary containing path information.
+    index (int): Index of the pathway.
+    global_frame (ReferenceFrame): The global reference frame.
+    suspension_body (RigidBody): The suspension body.
+
+    Returns:
+    tuple: A tuple containing the system and matrix A.
+    """
     point1 = linpath['paths'][index].attachments[0]
     point2 = linpath['paths'][index].attachments[1]
-    
-    print(point1, point2)
-    
+
     bodies_str = ''.join([point1.name[0], point2.name[0]])
-    
+
     body1 = linpath['bodies'][0]
     body2 = linpath['bodies'][-1]
-    
+
     mass1 = linpath['bodies'][0].mass
-    mass1 = linpath['bodies'][1].mass
-   
+    mass2 = linpath['bodies'][1].mass
+
     gb_frame = global_frame
-    ind_str = index+1
+    ind_str = index + 1
     l1 = symbols('l1', positive=True)
-    L = symbols('L_' + bodies_str+ '{}{}'.format(index+1, index+1), positive=True)
-    F,g,t = symbols('F, g, t')
-    
-    
-    frames = symbols('N{}{}_{}1:{}'.format(ind_str, ind_str,bodies_str,n+2), cls=ReferenceFrame)
-    angles = dynamicsymbols('alpha{}{}_{}1:{}'.format(ind_str, ind_str,bodies_str,n+2))
-    betas = symbols('beta{}{}_{}1:{}'.format(ind_str, ind_str,bodies_str,n+2))
-    #qs = dynamicsymbols('q1:{}'.format(3*(n+1)+1))
-    qs = dynamicsymbols('q{}{}_{}1:{}'.format(ind_str, ind_str,bodies_str, 3*(n+1)+1))
-    us = dynamicsymbols('u{}{}_{}1:{}'.format(ind_str, ind_str,bodies_str, 3*(n+1)+1))
-    print(qs)
-    masses = symbols('m{}{}_{}1:{}'.format(ind_str, ind_str, bodies_str, n+2))
+    L = symbols(f'L_{bodies_str}{ind_str}{ind_str}', positive=True)
+    F, g, t = symbols('F, g, t')
+
+    frames = symbols(f'N{ind_str}{ind_str}_{bodies_str}1:{n+2}', cls=ReferenceFrame)
+    angles = dynamicsymbols(f'alpha{ind_str}{ind_str}_{bodies_str}1:{n+2}')
+    betas = symbols(f'beta{ind_str}{ind_str}_{bodies_str}1:{n+2}')
+    qs = dynamicsymbols(f'q{ind_str}{ind_str}_{bodies_str}1:{3*(n+1)+1}')
+    us = dynamicsymbols(f'u{ind_str}{ind_str}_{bodies_str}1:{3*(n+1)+1}')
+
+    masses = symbols(f'm{ind_str}{ind_str}_{bodies_str}1:{n+2}')
     M = symbols('M')
-    
-    
+
     model = suspension_body.model
-    
-    op_vals = {qs[i]:0 for i in range(0,3*(n+1),3)} | \
-    {qs[i]:0 for i in range(1,3*(n+1),3)} | \
-    {qs[i]:-(i-i//3)*l1/2 for i in range(2,3*(n+1),3)} | \
-    {F:mass1*g} 
-    
-    
+
+    op_vals = (
+        {qs[i]: 0 for i in range(0, 3*(n+1), 3)}
+        | {qs[i]: 0 for i in range(1, 3*(n+1), 3)}
+        | {qs[i]: -(i - i//3)*l1/2 for i in range(2, 3*(n+1), 3)}
+        | {F: mass1 * g}
+    )
+
     dist21 = point2.pos_from(point1).express(gb_frame)
     distx = dist21.dot(gb_frame.x)
     disty = dist21.dot(gb_frame.y)
     distz = dist21.dot(gb_frame.z)
-    
-    angle = distz/dist21.magnitude()
+
+    angle = distz / dist21.magnitude()
     angles[-1] = angle - np.sum([i for i in angles[:-1]])
-    
-    
+
     S = suspension_body
     wall = RigidBody('W', masscenter=S.global_origin, frame=gb_frame)
     system = System.from_newtonian(wall)
-    
-    #print(angles)
-    [i.orient_axis(system.frame, j, system.frame.z) for i,j in list(zip(frames, angles))]
-    
-    
-    ps = symbols('P{}{}_{}1:{}'.format(ind_str, ind_str, bodies_str, n+2), cls=Point)
 
+    for i, j in zip(frames, angles):
+        i.orient_axis(system.frame, j, system.frame.z)
 
+    ps = symbols(f'P{ind_str}{ind_str}_{bodies_str}1:{n+2}', cls=Point)
 
-    # Setting these points in space
-    j=0
-    for i in range(n+1):
-        print(ps[i], qs[j], qs[j+1], qs[j+2])
-        ps[i].set_pos(point1, qs[j]*gb_frame.x + qs[j+1]*gb_frame.y + qs[j+2]*gb_frame.z)
-        j+=3
-
-    
-    op_point_wire = {qs[-3]:distx, qs[-2]:disty, qs[-1]:distz}
-    
-    
-    # Creating the bodies
-    bodies = [RigidBody('B{}{}_{}{}'.format(ind_str, ind_str,bodies_str,h), mass=i*j, masscenter=k, frame=l)
-              for h,i,j,k,l in list(zip(range(1,n+2), betas,masses, ps, frames))]
-    
- 
-    # Adding bodies to the system
-    for i in bodies:
-        system.add_bodies(i)
-        
-    # Assigning velocities to the bodies
     j = 0
     for i in range(n+1):
-        print(frames[i], bodies[i])
-        bodies[i].masscenter.set_vel(system.frame, 
-                                     us[j]*gb_frame.x + us[j+1]*gb_frame.y + us[j+2]*gb_frame.z)
+        ps[i].set_pos(point1, qs[j]*gb_frame.x + qs[j+1]*gb_frame.y + qs[j+2]*gb_frame.z)
+        j += 3
+
+    op_point_wire = {
+        qs[-3]: distx, qs[-2]: disty, qs[-1]: distz
+    }
+
+    bodies = [
+        RigidBody(
+            f'B{ind_str}{ind_str}_{bodies_str}{h}', mass=i*j,
+            masscenter=k, frame=l
+        )
+        for h, i, j, k, l in zip(range(1, n+2), betas, masses, ps, frames)
+    ]
+
+    for body in bodies:
+        system.add_bodies(body)
+
+    j = 0
+    for i in range(n+1):
+        bodies[i].masscenter.set_vel(
+            system.frame,
+            us[j]*gb_frame.x + us[j+1]*gb_frame.y + us[j+2]*gb_frame.z
+        )
         bodies[i].frame.set_ang_vel(frames[i], 0)
-        j+=3
-        
-        
+        j += 3
 
-    #print(list(zip(qs, us)))
-    for k in list(zip(qs, us)):
-        #print(qs)
-        system.add_coordinates(k[0])
-        system.add_speeds(k[1])
-        system.add_kdes(k[1]-k[0].diff(t))
-        
-    # Applying gravity
+    for q, u in zip(qs, us):
+        system.add_coordinates(q)
+        system.add_speeds(u)
+        system.add_kdes(u - q.diff(t))
+
     system.apply_uniform_gravity(-g * wall.z)
-    
-   
-    
-    print(ps[-1].pos_from(point2).express(gb_frame).subs({qs[-3]:distx, qs[-2]:disty, qs[-1]:distz}).subs(model.op_point))
-    
 
-    
-    linear_points = [point1] + [i for i in ps]# + [point2]
-    print(linear_points)
-    
+    linear_points = [point1] + [i for i in ps]
+
     linear_paths = []
-    for i in range(1, len(linear_points)-1):
-        lin_paths_a = LinearPathway(linear_points[i-1], linear_points[i])
-        lin_paths_b = LinearPathway(linear_points[i+1], linear_points[i])
+    for i in range(1, len(linear_points) - 1):
+        lin_paths_a = LinearPathway(linear_points[i - 1], linear_points[i])
+        lin_paths_b = LinearPathway(linear_points[i + 1], linear_points[i])
         linear_paths.append(lin_paths_a)
         linear_paths.append(lin_paths_b)
+
     linear_paths.append(LinearPathway(linear_points[-2], linear_points[-1]))
-    print(linear_paths)
-    [system.add_loads(i.to_loads(-F)[1]) for i in linear_paths[:-1]]
-    
+
+    for path in linear_paths[:-1]:
+        system.add_loads(path.to_loads(-F)[1])
+
     system.validate_system()
-    
-    # Getting the kane's equations
+
     system.form_eoms()
     kanel = system.eom_method.to_linearizer()
-    
-    
+
     A, B = kanel.linearize(A_and_B=True, op_point=op_vals)
-    
-    A = A.subs({l1: L/(n+1)}) 
-    B = B.subs({l1:L/(n+1)}) 
-    
-    
+
+    A = A.subs({l1: L/(n+1)})
+    B = B.subs({l1: L/(n+1)})
+
     return system, A
+
